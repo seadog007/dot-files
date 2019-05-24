8f64d4ab3b8fffd32123a960184b6a1ce5995641
Jasper Yu <007seadog@gmail.com>
2019-05-24 19:15:46 +0000

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-git clone https://github.com/seadog007/dot-files.git
+# Run this script by
+# curl https://raw.githubusercontent.com/seadog007/dot-files/master/setup.sh | bash
 
-cd dot-files
+git clone https://github.com/seadog007/dot-files.git /tmp/dot-files
+
+cd /tmp/dot-files
 cp .ssh/config >> ~/.ssh/config
 cat .ssh/authorized_keys >> ~/.ssh/authorized_keys
 cp .gitconfig ~/
 cp .tmux.conf ~/
-cp .bashrc ~/
+[ "`basename $SHELL`" == "bash" ] && cp .bashrc ~/
 cp .inputrc ~/
 
-git clone https://github.com/scwuaptx/peda.git ~/.peda && echo "source ~/.peda/peda.py" >> ~/.gdbinit
+if [ "`uname -a | cut -d\  -f1`" == "Linux" ]
+then
+	cd
+	[ -d "~/.peda" ] && cd ~/.peda && git pull
+	[ ! -d "~/.peda" ] && git clone https://github.com/scwuaptx/peda.git ~/.peda && echo "source ~/.peda/peda.py" >> ~/.gdbinit
+fi
 
 cd
-rm -rf .vim .vimrc
-git clone https://github.com/seadog007/dot-files-vim.git .vim && ln -s .vim/.vimrc
+[ -d "~/.vim" ] && cd ~/.vim && git pull
+[ ! -d "~/.vim" ] && git clone https://github.com/seadog007/dot-files-vim.git .vim && ln -s .vim/.vimrc
+
+rm -rf /tmp/dot-files
